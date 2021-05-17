eb79fd2509a506867c7b014fa050683d90eb4628
Wang Shijiang <shijiangjiayou@163.com>
2021-05-17 11:28:43 +0800

    添加测试文章

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,45 @@
 import React from "react"
+import { Link, graphql } from 'gatsby'
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout defKey="home">
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site with ant design! (antd)</p>
-    <p>You can find all Ant Design components <a href="https://ant.design/components/button/">here</a></p>
-    <p>Scroll down if you haven't already!</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link to={node.fields.slug}>
+          <h3>
+            {node.frontmatter.title}{" "}
+            <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </Link>
+      </div>
+    ))}
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
