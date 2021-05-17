2d21861a09757a3d772741aeb7323e0252c463bd
Wang Shijiang <shijiangjiayou@163.com>
2021-05-17 21:22:09 +0800

    添加底部标签跳转

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import _ from 'lodash';
 
 const Footer = () => {
   const { allMarkdownRemark = { group: [] } } = useStaticQuery(
@@ -18,19 +19,22 @@ const Footer = () => {
   return (
     <footer className="main-footer">
       <div className="container">
-        <div className="footer__topics open">
-          <h4 className="footer__topics__title" id="topics">查看所有主题的文章</h4>
+        <div className="footer__topics">
+          <h4 className="footer__topics__title" id="topics">查看所有分类的文章</h4>
           <ul className="footer__topics__list">
             {
               allMarkdownRemark.group.map((tagItem, index) => {
                 return (
                   <li className="footer__topics__item">
-                    <Link key={index}>{tagItem.tag}</Link>
+                    <Link to={`/tags/${_.kebabCase(tagItem.tag)}/`} key={index}>{tagItem.tag}</Link>
                   </li>
                 )
               })
             }
           </ul>
+        </div>
+        <div className="copy-right">
+          Firefly Laboratory  &copy; 2021
         </div>
       </div>
     </footer>
