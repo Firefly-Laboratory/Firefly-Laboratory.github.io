25a493e0d837d299292e65a57e227e0fd2d8a106
Wang Shijiang <shijiangjiayou@163.com>
2021-05-17 14:35:49 +0800

    调整顶部菜单，抽取到单独的组件

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,11 @@
-import React, { useState, useLayoutEffect } from "react"
+import React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
-import { Layout as AntdLayout, Menu, Row, Col } from "antd"
-import Toggle from './Toggle';
-import sun from '../assets/sun.png';
-import moon from '../assets/moon.png';
-import firefly from '../assets/firefly.png';
+import { Layout as AntdLayout } from "antd"
+import Navigation from './navigation';
 
 const { Header, Footer, Content } = AntdLayout
 
 const Layout = ({ defKey, children }) => {
-  const [theme, setTheme] = useState(null);
-
-  useLayoutEffect(() => {
-    setTheme(window.__theme);
-    window.__onThemeChange = () => {
-      setTheme(window.__theme);
-    };
-  });
-
   return (
     <AntdLayout>
       <Header
@@ -30,66 +17,7 @@ const Layout = ({ defKey, children }) => {
           padding: 0,
         }}
       >
-        <Row style={{ flexFlow: 'nowrap', height: 64 }}>
-          <Col xxl={4} xl={5} lg={6} md={6} sm={24} xs={24}>
-            <Link to="/" id="logo">
-              <img alt="logo" className="logo" src={firefly} />
-              Firefly Laboratory
-            </Link>
-          </Col>
-          <Col xxl={20} xl={19} lg={18} md={18} sm={24} xs={24} className="menu-row">
-            <Menu
-              mode="horizontal"
-              selectedKeys={[defKey]}
-              id="nav"
-            >
-              <Menu.Item key="home">
-                <Link to="/">Home</Link>
-              </Menu.Item>
-              <Menu.Item key="page2">
-                <Link to="/page-2/">Page 2</Link>
-              </Menu.Item>
-              <Menu.Item key="404">
-                <Link to="/404/">Secret page</Link>
-              </Menu.Item>
-            </Menu>
-            <div className="header-white-space" />
-            <div className="header-button header-switch-button">
-              {theme !== null ? (
-                <Toggle
-                  icons={{
-                    checked: (
-                      <img
-                        src={moon}
-                        width="16"
-                        height="16"
-                        role="presentation"
-                        style={{ pointerEvents: 'none' }}
-                      />
-                    ),
-                    unchecked: (
-                      <img
-                        src={sun}
-                        width="16"
-                        height="16"
-                        role="presentation"
-                        style={{ pointerEvents: 'none' }}
-                      />
-                    ),
-                  }}
-                  checked={theme === 'dark'}
-                  onChange={e =>
-                    window.__setPreferredTheme(
-                      e.target.checked ? 'dark' : 'light'
-                    )
-                  }
-                />
-              ) : (
-                <div style={{ height: '24px' }} />
-              )}
-            </div>
-          </Col>
-        </Row>
+        <Navigation defKey={defKey} />
       </Header>
       <Content
         style={{
---
+++ src/components/navigation.js
@@ -0,0 +1,83 @@
+import React, { useLayoutEffect, useState } from 'react'
+import { Link } from 'gatsby';
+import { Menu, Row, Col } from 'antd';
+import Toggle from './toggle';
+import sun from '../assets/sun.png';
+import moon from '../assets/moon.png';
+import firefly from '../assets/firefly.png';
+
+const Navigation = ({ defKey }) => {
+  const [theme, setTheme] = useState(null);
+
+  useLayoutEffect(() => {
+    setTheme(window.__theme);
+    window.__onThemeChange = () => {
+      setTheme(window.__theme);
+    };
+  });
+
+  return (
+    <Row style={{ flexFlow: 'nowrap', height: 64 }}>
+      <Col xxl={4} xl={5} lg={6} md={6} sm={24} xs={24}>
+        <Link to="/" id="logo">
+          <img alt="logo" className="logo" src={firefly} />
+          Firefly Laboratory
+        </Link>
+      </Col>
+      <Col xxl={20} xl={19} lg={18} md={18} sm={24} xs={24} className="menu-row">
+        <Menu
+          mode="horizontal"
+          selectedKeys={[defKey]}
+          id="nav"
+        >
+          <Menu.Item key="home">
+            <Link to="/">首页</Link>
+          </Menu.Item>
+          <Menu.Item key="articles">
+            <Link to="/articles/">文章</Link>
+          </Menu.Item>
+          <Menu.Item key="guides">
+            <Link to="/guides/">教程</Link>
+          </Menu.Item>
+        </Menu>
+        <div className="header-white-space" />
+        <div className="header-button header-switch-button">
+          {theme !== null ? (
+            <Toggle
+              icons={{
+                checked: (
+                  <img
+                    src={moon}
+                    width="16"
+                    height="16"
+                    role="presentation"
+                    style={{ pointerEvents: 'none' }}
+                  />
+                ),
+                unchecked: (
+                  <img
+                    src={sun}
+                    width="16"
+                    height="16"
+                    role="presentation"
+                    style={{ pointerEvents: 'none' }}
+                  />
+                ),
+              }}
+              checked={theme === 'dark'}
+              onChange={e =>
+                window.__setPreferredTheme(
+                  e.target.checked ? 'dark' : 'light'
+                )
+              }
+            />
+          ) : (
+            <div style={{ height: '24px' }} />
+          )}
+        </div>
+      </Col>
+    </Row>
+  );
+}
+
+export default Navigation
